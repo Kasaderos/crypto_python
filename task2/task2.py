53a9096aa174d3f14f0ca46111bfa3cc90e85a22
kasaderos <kb.kaldarov@yandex.ru>
2019-12-20 23:38:57 +0300

    task2 fixed

--- task2/task2.py
+++ task2/task2.py
@@ -1,76 +1,109 @@
 import random
 import sys
+import argparse
+import json
 
-# для сравнения чисел
-EPS = 1e-6
 
-def gen_key():
-    with open("keygen", "w") as f:
-        key = random.sample(range(256), 256)
-        f.write(' '.join([str(k) for k in key]))
+def gen_key(keygen):
+    key = random.sample(range(256), 256)
+    text = json.dumps({i: key[i] for i in range(256)})
+    write_file(keygen, text)
 
 
 def write_file(name, text):
     with open(name, "w") as out:
         out.write(text)
 
-def encrypt(filename, key):
-    with open(filename, "r") as f:
-        try:
-            write_file("cipher", ''.join([str(chr(key[ord(ch)])) for ch in f.read()]))
-        except:
-            print("symbol size() > 1 byte")
 
-def get_key_from_file():
-    with open("keygen", "r") as f:
-        return [int(k) for k in f.read().split()]
+def get_text(file):
+    with open(file, "r") as f:
+        return f.read()
+
+
+def encrypt(filename, keygen):
+    try:
+        key = get_key(keygen)
+        write_file("cipher", ''.join(
+            [str(chr(key[ord(ch)])) for ch in get_text(filename)]))
+    except:
+        print("symbol size() > 1 byte")
+
+
+def get_key(keygen):
+    d = json.loads(get_text(keygen))
+    return {int(k): d[k] for k in d}
 
 
 def get_frequency(filename):
-    with open(filename, "r") as f:
-        text = f.read()
-        count = {ord(ch) : text.count(ch) for ch in text}
-        freq = {k : count[k]/len(text) for k in count.keys() if count[k] != 0}
-        return freq, text
-
-
-def get_possible_keys(p1, p2):
-    l1 = list(p1.keys())
-    l2 = list(p2.keys())
-    possible = {l1[i] : l2[i] for i in range(len(l1))} 
-    return possible
+    text = get_text(filename)
+    count = {ord(ch): text.count(ch) for ch in text}
+    print(sortFreq(count))
+    freq = {k: count[k]/len(text) for k in count.keys() if count[k] != 0}
+    return freq
 
 
 def sortFreq(p):
-    return {k: v for k, v in sorted(p.items(), key=lambda item: item[1])}
+    return sorted(p.items(), key=lambda kv: kv[1])
 
-
-def write_decipher(cipher, keys):    
-    deciphered = ''.join([str(chr(keys[ord(ch)])) for ch in cipher])
+def decrypt(cipher, keygen):
+    keygen = get_key(keygen)
+    keys = {keygen[k]: k for k in keygen}
+    deciphered = ''.join([str(chr(keys[ord(ch)])) for ch in get_text(cipher)])
     write_file("deciphered.txt", deciphered)
 
 
-def decrypt(text, cipher):
-    p1, cipher = get_frequency(cipher)
-    p2, text = get_frequency(text)
+def create_model(inputfile, cipher):
+    p1 = get_frequency(inputfile)
+    p2 = get_frequency(cipher)
     p1 = sortFreq(p1)
     p2 = sortFreq(p2)
-    assert(len(p1)==len(p2))
-    keys = get_possible_keys(p1, p2)
-    write_file("possible_key", str(keys))
-    write_decipher(cipher, keys)
+    assert(len(p1) == len(p2))
+    text = json.dumps(p1) + '\n' + json.dumps(p2)
+    write_file('model', text)
 
-if __name__ == "__main__":    
-    if "-g" in sys.argv and len(sys.argv) == 2:
-        # python3 task2.py -g 
+
+def get_possible_keys(p1, p2):
+    l1 = [l[0] for l in p1]
+    l2 = [l[0] for l in p2]
+    possible = {l1[i]: l2[i] for i in range(len(l1))}
+    return possible
+
+
+def break_key(model):
+    p1, p2 = get_text(model).split('\n')
+    p1 = json.loads(p1)
+    p2 = json.loads(p2)
+    keys = get_possible_keys(p1, p2)
+    write_file("possible_key", json.dumps(keys))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--generate', nargs=1)
+    parser.add_argument('-e', '--encrypt', nargs=2)
+    parser.add_argument('-d', '--decrypt', nargs=2)
+    parser.add_argument('-m', '--model', nargs=2)
+    parser.add_argument('-b', '--break', nargs=1)
+    args = vars(parser.parse_args())
+
+    if args['generate']:
+        # python3 task2.py -g keygen
         # генерация и запись ключа в файл keygen
-        gen_key()
-    if "-e" in sys.argv and len(sys.argv) == 3:
-        # python3 task2.py -e in.txt
+        gen_key(args['generate'][0])
+    elif args['encrypt']:
+        # python3 task2.py -e in.txt keygen
         # шифрование по ключу keygen
-        encrypt(sys.argv[2], get_key_from_file())
-    if "-d" in sys.argv and len(sys.argv) == 4:
-        # python3 task2.py -d in.txt cipher
-        # взлом и запись расшифрованного текста
-        decrypt(sys.argv[2], sys.argv[3])
-    
+        encrypt(args['encrypt'][0], args['encrypt'][1])
+    elif args['decrypt']:
+        # python3 task2.py -d cipher keygen
+        decrypt(args['decrypt'][0], args['decrypt'][1])
+
+    elif args['model']:
+        # python3 task2.py -m in.txt cipher
+        # модель
+        create_model(args['model'][0], args['model'][1])
+    elif args['break']:
+        # python3 task2.py -b model
+        # взлом
+        break_key(args['break'][0])
